dcd021c2be9764af389a5da1c6f893fb0195e66a
Troy Hepper <heppertroy@gmail.com>
2020-04-24 19:19:28 -0400

    updating for github pages deployment:

---
+++ precache-manifest.ad533feb444d9617636078dac571d58b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b287328d7e66041a957da28d9a872e37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "1c345d99c45fdd05b11d",
+    "url": "/static/css/main.9768e7c3.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c345d99c45fdd05b11d",
+    "url": "/static/js/main.e640d731.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
